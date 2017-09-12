950c36634d9cbbc84106281403406b4965a3677a
sirirak <hotmail22866@gmail.com>
2017-09-13 02:58:33 +0700

    setup view and view holder

---
+++ recycler_view_sleepingfotless/src/main/java/com/example/minato/template/TitleViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.minato.template
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.view_title.view.*
+
+/**
+ * Created by minato on 9/13/2017.
+ */
+class TitleViewHolder(itemView:View) : RecyclerView.ViewHolder( itemView) {
+    val tvTitle = itemView.tv_title
+}
